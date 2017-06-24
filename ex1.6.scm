3e4f8fcee3fdbd48cdd6ba8c929483d96d2a51a4
jestuber <jestuber@gmail.com>
2017-06-24 12:29:11 -0700

    Create ex1.6.scm

---
+++ ex1.6.scm
@@ -0,0 +1,29 @@
+;Exercise 1.6.  Alyssa P. Hacker doesn't see why if needs to be provided as a special form. ``Why can't I just define it as an ordinary procedure in terms of cond?'' she asks. Alyssa's friend Eva Lu Ator claims this can indeed be done, and she defines a new version of if:
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+      
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+;What happens when Alyssa attempts to use this to compute square roots? Explain.
+
+; scheme uses applicative order evaluation, so it evaluates both
+; the then-clause and the else-clause. The else-clause keeps calling itself
+; and runs forever.
+; The special if statement uses normal order evaluation?
+
+
+
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
