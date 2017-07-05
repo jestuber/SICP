e68ecef898b976268c246dc766207b0e624d7ca8
jestuber <jestuber@gmail.com>
2017-07-04 18:48:35 -0700

    Create ex1.22.scm

---
+++ ex1.22.scm
@@ -0,0 +1,39 @@
+; ex1.22 
+; Using this procedure, write a procedure search-for-primes that checks the primality of consecutive odd integers in a specified range. Use your procedure to find the three smallest primes larger than 1000; larger than 10,000; larger than 100,000; larger than 1,000,000. Note the time needed to test each prime. Since the testing algorithm has order of growth of (n), you should expect that testing for primes around 10,000 should take about 10 times as long as testing for primes around 1000. Do your timing data bear this out? How well do the data for 100,000 and 1,000,000 support the n prediction? Is your result compatible with the notion that programs on your machine run in time proportional to the number of steps required for the computation?
+
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes start end)
+   (if (even? start)
+       (search-for-primes (+ start 1) end)
+       (cond ((< start end) (timed-prime-test start)
+                            (search-for-primes (+ start 2) end))))
+                          
+(define (prime? n)
+   (= n (smallest-divisor n)))
+ 
+(define (smallest-divisor n)
+   (find-divisor n 2))
+ 
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+      
+(define (divides? a b)
+  (= (mod b a) 0))
+
+(define (square x)
+  (* x x))
+  
+; need to find a version of scheme that has runtime or equivalent
